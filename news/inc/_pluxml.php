a7bad82a22f2b431c5f2c6a12a036fa0506c0470
sudwebdesign <thomas@sudwebdesign.fr>
2017-03-21 14:49:04 +0100

    Fix bad redir
    Mauvaise redirection après l'authentification

--- news/inc/_pluxml.php
+++ news/inc/_pluxml.php
@@ -71,6 +71,6 @@ $plxShow = plxShow::getInstance();
 if(isset($_SESSION['user']) AND !empty($_SESSION['user'])) {
 	$_profil = $plxMotor->aUsers[$_SESSION['user']];
 } else {# rediriger a l'authentification si non conncté a Pluxml
-	header('Location:../../../core/admin/auth.php?p=/caf/core/admin/plugin.php?p=gutuma');
+	header('Location:../../../core/admin/auth.php?p=plugin.php?p=gutuma');
 	exit();
 }
