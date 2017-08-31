280c4a6cf028ab7e5867dedb05275f4761d613fd
sudwebdesign <thomas@sudwebdesign.fr>
2017-08-31 05:24:03 +0200

    add english admin

---
+++ lang/en.php
@@ -0,0 +1,23 @@
+<?php 
+/**
+ * Fichier de traduction pour gutuma
+ *
+ * @version 1.3
+ * @date	29/10/2012
+ * @author	Cyril MAGUIRE
+ **/
+	$LANG = array(
+		'L_GUTUMA_INSTALL'      =>	'Install module',
+		'L_WRITE_NEWS'          =>	'Go to the newsletters module',
+		'L_ACTIVATE_USER'       =>	'Activate user',
+		'L_Activé'              =>	'Active',
+		'L_Desactivé'           =>	'Deactivated',
+		'L_'                    =>	'Deactivated',//1st install or user never activated
+		'L_NOT_APPLICABLE'      =>	'Unapplicable',
+		'L_DEL_USER'            =>	'Deactivate user',
+		'L_DESCRIPTION0'        =>	'Allows to manage the users with the possibility of posting of newsletters<br />(administrators or managers).',//4 admin
+		'L_DESCRIPTION1'        =>	'Allows access for posting and manage newsletters. <noscript>If activate javascript, Go to the newsletters module directly</noscript>',//4 manager
+		'L_SEE_ADMIN'           =>	'See with an administrator to activate your account.',
+		'L_ERR_EMAIL'           =>	'Please enter an email address in 001 profile Admin',
+		'L_INSTALL_FIRST'       => 'Module not installed'
+	);
