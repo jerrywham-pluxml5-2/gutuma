a170608b5118ab6744f5a30dae2621b4a1c3916c
sudwebdesign <thomas@sudwebdesign.fr>
2017-08-31 06:19:42 +0200

    Improve _pluxml GROOT algo

--- news/inc/_pluxml.php
+++ news/inc/_pluxml.php
@@ -11,18 +11,13 @@
 #
 # ------------------- END LICENSE BLOCK -------------------
 /* Gutama plugin package (from core/admin/prepend.php)
- *  @version 1.4
+ * @version 1.4
  * @date	23/09/2013
  * @author	Cyril MAGUIRE
 */
 
 # Définition des constantes
-$gu_real_path = str_replace('plugins/gutuma/news/inc/_pluxml.php','',__FILE__);
-$gu_call_path = str_replace($_SERVER['PHP_SELF'],'',$_SERVER['SCRIPT_FILENAME']);
-//duckduckgo.com/?t=lm&q=PHP+SOLVE+FOLDER+SYMLINK&ia=qa
-$output = array();
-exec('pwd', $output);
-define('__LINK__', $output[0].substr(__FILE__, strpos(__FILE__, DIRECTORY_SEPARATOR)));
+$output = array();exec('pwd', $output);define('__LINK__', $output[0].substr(__FILE__, strpos(__FILE__, DIRECTORY_SEPARATOR)));//in gadget.js.php
 $gdgt='';
 if(strstr($_SERVER['PHP_SELF'],'gadgets.js.php')){//4 gadget call
 	$gdgt='../';
@@ -30,7 +25,7 @@ if(strstr($_SERVER['PHP_SELF'],'gadgets.js.php')){//4 gadget call
 }
 define('PLX_ROOT',$gdgt.'../../../');
 define('PLX_CORE', PLX_ROOT.'core/');
-define('PLX_GROOT', $gu_call_path.'/');
+define('PLX_GROOT', str_replace($_SERVER['PHP_SELF'],'',$_SERVER['SCRIPT_FILENAME']).'/');//gu_call_path :) duckduckgo.com/?t=lm&q=PHP+SOLVE+FOLDER+SYMLINK&ia=qa ::: 4 memory : $gu_real_path = str_replace('plugins/gutuma/news/inc/_pluxml.php','',__FILE__);
 define('PLX_MORE', PLX_GROOT.'core/');
 include(PLX_GROOT.'config.php');
 include(PLX_MORE.'lib/config.php');
@@ -43,15 +38,15 @@ if(!file_exists(path('XMLFILE_PARAMETERS'))) {
 session_start();
 
 $session_domain = dirname(__FILE__);
-/*
-if(!defined('PLX_AUTHPAGE') OR PLX_AUTHPAGE !== true){ # si on est pas sur la page de login
-	# Test sur le domaine et sur l'identification
-	if((isset($_SESSION['domain']) AND $_SESSION['domain']!=$session_domain) OR (!isset($_SESSION['user']) OR $_SESSION['user']=='')){
-		header('Location: auth.php?p='.htmlentities($_SERVER['REQUEST_URI']));
-		exit;
-	}
-}
-*/
+#::see bottom of this file:: This modified original code of PluXml is Commented because bad redirect pages & ajax / subscribe / gadget in public mode to plugins/gutuma/news/auth.php?p=plugins/gutuma/news/*pageRequested*.php
+#if(!defined('PLX_AUTHPAGE') OR PLX_AUTHPAGE !== true){ # si on est pas sur la page de login
+#	# Test sur le domaine et sur l'identification
+#	if((isset($_SESSION['domain']) AND $_SESSION['domain']!=$session_domain) OR (!isset($_SESSION['user']) OR $_SESSION['user']=='')){
+#		header('Location: '.PLX_CORE.'admin/auth.php?p='.htmlentities($_SERVER['REQUEST_URI']));//add '.PLX_CORE.'admin/ 4 good page url but p query param is'nt recognized after redirected
+#		exit;
+#	}
+#}
+
 # On inclut les librairies nécessaires
 include_once(PLX_MORE.'lib/class.plx.date.php');
 include_once(PLX_MORE.'lib/class.plx.glob.php');
@@ -78,16 +73,16 @@ $_SESSION['lang'] = $_SESSION['glang'] = $lang;
 # Chargement des fichiers de langue
 loadLang(PLX_MORE.'lang/'.$lang.'/core.php');
 loadLang(PLX_MORE.'lang/'.$lang.'/admin.php');
-$plxMotor->mode='gutuma';
-//$plxMotor->prechauffage();##origin //bug header 404 when ? is in uri (admin) el motor go to mode error, sino mode home ;-) [non blocant] semble être inutile
-$plxMotor->demarrage();#origin semble inutile ici
+$plxMotor->mode='gutuma';//4 future ::: & solved bug header 404 in demarrage & prechauffage funct() but in reality is'nt util here (more perf ;-)
+#$plxMotor->prechauffage();#origin //bug header 404 when ? is in uri (admin) el motor go to mode error, sin mode home ;-) [non blocant] semble être inutile
+#$plxMotor->demarrage();#origin semble inutile de l'appeler maintenant (maybe not: intest)
 # Creation de l'objet d'affichage
 $plxShow = plxShow::getInstance();
 if(isset($_SESSION['user']) AND !empty($_SESSION['user'])) {
 	$_profil = $plxMotor->aUsers[$_SESSION['user']];// _profil ONLY CALLED IN INSTALL.PHP
 	$plxMotor->mode='gutumadmin';
-
-}elseif(strpos($plxMotor->path_url,'news/ajax.php') === FALSE  && strpos($plxMotor->path_url,'news/js/gadgets.js.php') === FALSE && strpos($plxMotor->path_url,'news/subscribe.php') === FALSE){//gestion des abonnements (publics)
+}//grant access 4 public php files subscript mode ::: other redirect (if not connected in PluXml backend)
+elseif(strpos($plxMotor->path_url,'news/ajax.php') === FALSE  && strpos($plxMotor->path_url,'news/js/gadgets.js.php') === FALSE && strpos($plxMotor->path_url,'news/subscribe.php') === FALSE){//gestion des abonnements (publics)
 	header('Location:'.PLX_CORE.'admin/auth.php?p=plugin.php?p=gutuma');
 	exit();
 }
