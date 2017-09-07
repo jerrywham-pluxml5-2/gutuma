ef2fd877e548ac6b0cf8d88e45f893659d105acf
sudwebdesign <thomas@sudwebdesign.fr>
2017-09-07 06:31:35 +0200

    v1.8.5.plx.5.6
    Url & Code

--- news/up_git.js
+++ news/up_git.js
@@ -5,5 +5,5 @@
  */
 
 var gu_latest_version_num = 2017090207;
-var gu_latest_version_name = "1.8.5";
-var gu_latest_download_url = "https://raw.githubusercontent.com/jerrywham-pluxml5-2/gutuma/master/news/up_git.js";
+var gu_latest_version_name = "1.8.5.plx.5.6";
+var gu_latest_download_url = "https://github.com/jerrywham-pluxml5-2/gutuma/archive/v.1.8.5.plx.5.6.zip";
